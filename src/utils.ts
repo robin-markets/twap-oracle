60cc553346f48219ad8469529b607ee01b8886ff
0xLedgerwing <xledgerwing@gmail.com>
2026-03-10 21:39:05 +0100

    minor fixes

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,9 @@ export const COLLATERAL_ASSET_ID = BigInt.fromI32(0);
 export const PRICE_SCALE = BigInt.fromI32(1000000);
 export const INDEX_SET_YES = BigInt.fromI32(1);
 export const INDEX_SET_NO = BigInt.fromI32(2);
-export const PARENT_COLLECTION_ID = Bytes.fromHexString("0x0") as Bytes;
+export const PARENT_COLLECTION_ID = Bytes.fromHexString(
+  "0x0000000000000000000000000000000000000000000000000000000000000000"
+) as Bytes;
 export const CONDITIONAL_TOKENS_ADDRESS = Address.fromString(
   "0x4D97DCd97eC945f40cF65F87097ACe5EA0476045"
 );
@@ -15,8 +17,6 @@ export const COLLATERAL_WCOL = Address.fromString(
   "0x3A3BD7bb9528E159577F7C2e685CC81A765002E2"
 );
 
-export function tokenIndexId(conditionId: Bytes, index: i32): Bytes {
-  return Bytes.fromHexString(
-    conditionId.toHex().concat("-").concat(index.toString())
-  );
+export function tokenIndexId(conditionId: Bytes, index: i32): string {
+  return conditionId.toHex().concat("-").concat(index.toString());
 }
