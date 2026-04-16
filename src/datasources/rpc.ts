518e75351c38bafc2bf630b10e369774d0e9c10a
0xLedgerwing <xledgerwing@gmail.com>
2026-04-16 20:05:39 +0100

    Revert "added log"
    This reverts commit a1d697a3ec3b754bec2e1f17633c28e8ca311256.

--- src/datasources/rpc.ts
+++ src/datasources/rpc.ts
@@ -298,7 +298,6 @@ export class RpcDataSource {
             });
         }
 
-        console.log('Submitting Multicall3 transaction');
         const hash = await this.walletClient.writeContract({
             chain: polygon,
             address: MULTICALL3_ADDRESS,
