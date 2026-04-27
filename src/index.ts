ec1204765403fe0bb6278c26bbe7f3cb9b6c4b7d
0xLedgerwing <xledgerwing@gmail.com>
2026-04-27 08:07:37 +0100

    removed rate limit for now

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const config = loadConfig();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(rateLimit);
+//app.use(rateLimit);
 
 app.use('/twap', createTwapRouter(config));
 
